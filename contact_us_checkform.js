c0234b1800c26ea18a373278896acb80da8eeca2
Kiraly Peter <pkiraly@tesuji.eu>
2012-02-24 22:31:41 +0100

    Issue 428: /contact-us page fix.

--- contact_us_checkform.js
+++ contact_us_checkform.js
@@ -19,6 +19,13 @@ function CheckMultiple16(frm, name) {
   return false;
 }
 
+/**
+ * Checks the form16
+ *
+ * @param f
+ *
+ * @returns {Boolean}
+ */
 function CheckForm16(f) {
   var email_re = /[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/i;
   if (!email_re.test(f.email.value)) {
@@ -65,5 +72,85 @@ function CheckForm16(f) {
     false;
   }
 
+  setGFields();
+
   return false;
+}
+
+/**
+ * Extracts value from cookie
+ *
+ * @param text
+ *   The full text of cookie
+ * @param name
+ *   The variable name
+ * @param separator
+ *   The separator or values
+ *
+ * @returns {String}
+ *   The value of the variable
+ */
+function _uGC(text, name, separator) {
+  if (!text || text=="" || !name || name=="" || !separator || separator=="") {
+    return '-';
+  }
+  var i, i2, i3, c = '-';
+  i = text.indexOf(name);
+  i3 = name.indexOf('=') + 1;
+  if (i > -1) {
+    i2 = text.indexOf(separator, i);
+    if (i2 == -1) {
+      i2 = text.length;
+    }
+    c = text.substring((i+i3), i2);
+  }
+  return c;
+}
+
+/**
+ * Set the Google Analitics fields of the form
+ */
+function setGFields() {
+  //
+  var z = _uGC(document.cookie, '__utmz=', ';');
+  //
+  var source   = _uGC(z, 'utmcsr=', '|');
+  var medium   = _uGC(z, 'utmcmd=', '|');
+  var term     = _uGC(z, 'utmctr=', '|');
+  var content  = _uGC(z, 'utmcct=', '|');
+  var campaign = _uGC(z, 'utmccn=', '|');
+  var gclid    = _uGC(z, 'utmgclid=', '|');
+  //
+  if (gclid !="-") {
+    source = 'google';
+    medium = 'cpc';
+  }
+
+  var csegment = _uGC(document.cookie, '__utmv=', ';');
+  if (csegment != '-') {
+    var csegmentex = /[1-9]*?\.(.*)/;
+    csegment = csegment.match(csegmentex);
+    csegment = csegment[1];
+  }
+  else { 
+    csegment = '(not set)';
+  }
+
+  //
+  var a = _uGC(document.cookie, '__utma=', ';');
+  var aParts = a.split(".");
+  var nVisits = aParts[5];
+
+  try {
+    $('#CustomFields_19_16').val(source);
+    $('#CustomFields_20_16').val(medium);
+    $('#CustomFields_21_16').val(term);
+    $('#CustomFields_22_16').val(content);
+    $('#CustomFields_23_16').val(campaign);
+    $('#CustomFields_24_16').val(csegment);
+    $('#CustomFields_25_16').val(nVisits);
+  }
+  catch(err) { }
+
+  return true;
 }
